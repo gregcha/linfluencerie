8845bd44acfd6aa1c48bb1a51bfc7eedb696a8de
gcm.charles@gmail.com <gcm.charles@gmail.com>
2017-04-13 19:54:05 +0200

    Automated commit at 2017-04-13 17:54:04 UTC by middleman-deploy 1.0.0

---
+++ javascripts/contact.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $(function() {
+    $(".form").submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+        url: "//formspree.io/charles@linfluencerie.com",
+        method: "POST",
+        data: $(this).serialize(),
+        dataType: "json",
+        success: function(data){
+          $('#footer h3').html("Message Envoyé <i class='fa fa-check'></i>");
+          $('#footer h3 i').css('color', '#f0c808');
+          $('.form').trigger("reset");
+        },
+        error: function(){
+          $('#footer h3').html("Une erreur s'est produite<i class='fa fa-times'></i>");
+          $('#footer h3 i').css('color', '#DD1C1A');
+          $('.form').trigger("reset");
+        }
+      });
+    });
+  });
+});
---
+++ javascripts/misc.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('.nav-links a').on('click', function() {
+		var section = $(this).attr('href');
+		var speed = 1000;
+		$('html, body').animate( { scrollTop: $(section).offset().top }, speed );
+		return false;
+	});
+});
+
+jQuery(window).on('load', function(){
+		$('.expertise-grid').masonry({
+		// $('.expertise-grid').masonry('layout');
+	  itemSelector: '.expertise-single',
+	});
+});
+
+	
+	
